5e176d7f81f5b744c6058bdb03e436e652910950
ak98161615 <ak98161615@gmail.com>
2021-07-21 10:47:24 +0530

    Updates

---
+++ precache-manifest.02332eac31e1f79e0460782c7a6b32cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390b9305d3813822384b3a877c5d1fa2",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "142992b8d432ee285c54",
+    "url": "/netflix-clone/static/css/main.88f6bee8.chunk.css"
+  },
+  {
+    "revision": "d94122286c2ed7c2735b",
+    "url": "/netflix-clone/static/js/2.79c20a69.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.79c20a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142992b8d432ee285c54",
+    "url": "/netflix-clone/static/js/main.a03b64b1.chunk.js"
+  },
+  {
+    "revision": "b948bf8b867562944125",
+    "url": "/netflix-clone/static/js/runtime-main.66b8f367.js"
+  }
+]);
